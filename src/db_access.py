10c010b68cdfe8f167c7dccdc81255e485e922a3
Akira Kubo <akira.kubo@konicaminolta.com>
2023-08-25 23:56:37 +0900

    ﻿:green_heart:add parameter validation test
    Commit body...

--- src/db_access.py
+++ src/db_access.py
@@ -4,3 +4,8 @@ def db_access_body() -> str:
     # 本当は、ここにDBアクセスのいろんなのが書いてある
     return "db_access"
 
+def db_access_func_with_parameters(str1:str,int1:int) -> str:
+    return db_access_body_with_parameters(str1, int1)
+def db_access_body_with_parameters(str1:str,int1:int) -> str:
+    # ここにもDBアクセスのいろんなのが書いてある
+    return "db_access_with_parameters"
--- tests/test_dbaccess.py
+++ tests/test_dbaccess.py
@@ -8,3 +8,10 @@ def test_DBアクセスがある関数をmockで差し替える(mocker):
 
     mocker.patch("src.db_access.db_access_body", return_value="mock_access")
     assert "mock_access" == db.db_access_func()
+# TODO 引数が正しく呼び出されているか
+# 回数が正しく呼び出されているか
+
+def test_DBアクセスがある引数ありの関数を正しく呼び出されているか(mocker):
+    m = mocker.patch("src.db_access.db_access_body_with_parameters")
+    db.db_access_func_with_parameters("abc", 123)
+    m.assert_called_with("abc", 123)
