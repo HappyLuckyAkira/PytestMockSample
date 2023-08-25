a36f9b8c0373ea38933ad941a75461e72fb722be
Akira Kubo <akira.kubo@konicaminolta.com>
2023-08-25 23:34:31 +0900

    ﻿:green_heart:adapt mock to change return value
    Commit body...

--- src/db_access.py
+++ src/db_access.py
@@ -1,7 +1,6 @@
 def db_access_func() -> str:
-    """
-
-    :rtype: object
-    """
+    return db_access_body()
+def db_access_body() -> str:
     # 本当は、ここにDBアクセスのいろんなのが書いてある
     return "db_access"
+
--- tests/test_dbaccess.py
+++ tests/test_dbaccess.py
@@ -4,5 +4,7 @@ sys.path.append('../')
 import src.db_access as db
 
 
-def test_DBアクセスがある関数をmockで差し替える():
-    assert "db_access" == db.db_access_func()
+def test_DBアクセスがある関数をmockで差し替える(mocker):
+
+    mocker.patch("src.db_access.db_access_body", return_value="mock_access")
+    assert "mock_access" == db.db_access_func()
